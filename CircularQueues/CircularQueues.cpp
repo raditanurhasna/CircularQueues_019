9a652df1c794ee6c4169f0fa450dc8dfefce5bea
raditanurhasna <raditanurhasna1410@gmail.com>
2023-06-22 10:50:29 +0700

    Menambahkan elemen pada Antrian yang tidak penuh.

--- CircularQueues/CircularQueues.cpp
+++ CircularQueues/CircularQueues.cpp
@@ -21,8 +21,24 @@ public:
 		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
 			cout << "\NQueue overflow\n";
 			return;
-	
 		}
 
+		//cek apakah antrian kosong
+		if (FRONT == -1) {
+			FRONT = 0;
+			REAR = 0;
+		}
+		else {
+			//Jika REAR berada di posisi terakhir array, kembali ke awal array
+			if (REAR == max - 1)
+				REAR = 0;
+			else
+				REAR = REAR + 1;
+		}
+		queue_array[REAR] = num;
+
+	}
+
+	
 	}
 };
