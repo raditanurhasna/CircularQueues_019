86b05208e89dd4c0e89c78c5ac3d4f52a820555d
raditanurhasna <raditanurhasna1410@gmail.com>
2023-06-22 11:24:20 +0700

    Memanggil Queues dan char

--- CircularQueues/CircularQueues.cpp
+++ CircularQueues/CircularQueues.cpp
@@ -80,7 +80,7 @@ public:
 				FRONT_position++;
 			}
 			cout << endl;
-
+		}
 		else {
 			//Jika FRONT_Position > REAR Position, iterasi dari FRONT hingga akhir array
 			while (FRONT_position <= max - 1) {
@@ -88,10 +88,21 @@ public:
 				FRONT_position++;
 
 			}
-			
-			}
-		}
-		
-		
 
-};
+			FRONT_position = 0;
+
+			//Iterasi dari awal array hingga REAR
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << " ";
+				FRONT_position++;
+			}
+			cout << endl;
+		}
+
+	};
+
+	int main() {
+		Queues q;
+		char ch;
+	
+	}
