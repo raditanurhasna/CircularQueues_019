c2998285d4c8fd90c643c4db28da070f47de9690
raditanurhasna <raditanurhasna1410@gmail.com>
2023-06-22 10:42:11 +0700

    Digunakan untuk menyisipkan elemen pada antrian dan memeriksa apakah antrian penuh atau tidak.

--- CircularQueues/CircularQueues.cpp
+++ CircularQueues/CircularQueues.cpp
@@ -11,7 +11,16 @@ public:
 		REAR = -1;
 
 	}
+	void insert() {
+		int num;
+		cout << "Enter a number: ";
+		cin >> num;
+		cout << endl;
 
+		// cek apakah antrian penuh 
+		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+			cout << "\NQueue overflow\n";
+			return;
 	
 		}
 
